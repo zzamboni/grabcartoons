d6ea448cc12ec28251e9bc51f5c57ec8e03b9004
Diego Zamboni <diego@zzamboni.org>
2002-08-16 14:11:52 +0000

    * modules/00grabcartoons_output.pl: Moved print_* subroutines from the main program to modules/00grabcartoons_output.pl. This way it's easier to customize the output of the program.

---
+++ modules/00grabcartoons_output.pl
@@ -0,0 +1,38 @@
+# Output routines, here you can modify how the output page looks.
+
+sub print_header {
+  my $today=scalar localtime;
+  print <<EOF;
+<html>
+  <head>
+    <title>Daily Comics - $today</title>
+  </head>
+
+  <body bgcolor="white" text="black">
+    <h1>Daily Comics - $today</h1>
+EOF
+}
+
+sub print_footer {
+  print <<EOF;
+
+<hr>
+<small>This page was created automatically by <a href="http://grabcartoons.sourceforge.net/">grabcartoons</a></small>
+  </body>
+</html>
+EOF
+}
+
+sub print_section {
+  my ($name, $url, $mainurl, $err)=@_;
+  print "<hr>\n<h2>$name</h2>\n\n";
+  if ($err) {
+    print "<em>$err</em><p>\n\n";
+  }
+  else {
+    $mainurl=$url if !$mainurl;
+    print "<a href=\"$mainurl\"><img src=\"$url\" alt=\"Today's $name cartoon\" border=0></a><p>\n\n";
+  }
+}
+
+1;
