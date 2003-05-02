a50ac3e9a48dee8881ba41a932430d25f8921147
Benjamin Kuperman <kuperman@users.sourceforge.net>
2003-05-02 17:27:41 +0000

    Updated PhD comic to point to www.phdcomics.com

--- grabcartoons/modules/phd.pl
+++ grabcartoons/modules/phd.pl
@@ -1,11 +1,11 @@
 sub get_url_phd {
-  my $phdbase="http://phd.stanford.edu";
-  my $phdpage="$phdbase/comics.cfm";
+  my $phdbase="http://www.phdcomics.com/comics";
+  my $phdpage="$phdbase/comics.php";
   my $title="Piled Higher and Deeper";
   fetch_url($phdpage)
     or return (undef, $phdpage, $title);
   while (get_line()) {
-    if (/src="(archive\/[^.]+\.gif)"/) {
+    if (/src=(archive\/[^.]+\.gif)/) {
 	return ($phdbase."/$1", $phdpage, $title);
     }
   }
