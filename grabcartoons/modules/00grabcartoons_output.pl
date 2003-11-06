85db32dfb1f0452e50daa8660f597cbd6850b484
Diego Zamboni <diego@zzamboni.org>
2003-11-06 23:26:52 +0000

    * modules/pVp.pl (get_url_pvp): New module for pVp   (http://www.pvponline.com/), contributed by Scott Baker.
    * modules/00grabcartoons_output.pl (print_header): Patch from
      Scott Baker to make the output valid HTML 4.01 Transitional.

--- grabcartoons/modules/00grabcartoons_output.pl
+++ grabcartoons/modules/00grabcartoons_output.pl
@@ -3,10 +3,13 @@
 sub print_header {
   my $today=scalar localtime;
   print <<EOF;
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+  
 <html>
   <head>
     <title>Daily Comics</title>
   </head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 
   <body bgcolor="white" text="black">
     <h1>Daily Comics - $today</h1>
