00090206f5a8f09cf406f03d8a9cf791c4a4e7d9
Diego Zamboni <diego@zzamboni.org>
2005-07-17 15:19:51 +0000

    * modules/penny_arcade.pl (get_url_penny_arcade): Updated for new page format.

--- modules/penny_arcade.pl
+++ modules/penny_arcade.pl
@@ -5,8 +5,8 @@ sub get_url_penny_arcade {
   fetch_url($page)
     or return (undef, $page, $title);
   while (get_line()) {
-    if (m@src="(http://img.penny-arcade.com/\d{4}/\d{8}.\.(gif|jpg))"@i) {
-	return ("$1", $page, $title);
+    if (m@src="(/images/\d{4}/\d{8}.\.(gif|jpg))"@i) {
+	return ("$base$1", $page, $title);
     }
   }
   $err="Could not find image in $title"."'s page";
