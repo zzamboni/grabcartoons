d6b600312eaf311870b770a60d67c9cf6c500766
Diego Zamboni <diego@zzamboni.org>
2005-07-17 14:49:19 +0000

    Added module for "Toothpaste for Dinner", http://www.toothpastefordinner.com/

---
+++ grabcartoons/modules/toothpastefordinner.pl
@@ -0,0 +1,10 @@
+# Written by Diego Zamboni
+sub get_url_toothpastefordinner {
+    # This is easy, fixed URL
+    my $base="http://www.toothpastefordinner.com/";
+    my $page="$base";
+    return("$base/today.gif", $page, "toothpastefordinner");
+}
+
+
+1;
