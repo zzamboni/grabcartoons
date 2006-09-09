ffb6c7aeb91982587e13420a711c59054bbb3fa5
Diego Zamboni <diego@zzamboni.org>
2006-09-09 19:55:56 +0000

    Sadly, Dr. Fun also does not exist anymore

--- modules/dr_fun.pl
+++
@@ -1,9 +0,0 @@
-sub get_url_dr_fun {
-    # This is easy, fixed URL
-    my $drfunbase="http://metalab.unc.edu/Dave";
-    my $drfunpage="$drfunbase/drfun.html";
-    return("$drfunbase/Dr-Fun/latest.jpg", $drfunpage, "Dr. Fun");
-}
-
-
-1;
