2e7a38b03168b90037c4542d1b069c5f338e6ee1
Diego Zamboni <diego@zzamboni.org>
2006-09-09 21:18:37 +0000

    Updated for new site

--- grabcartoons/modules/mother_goose.pl
+++ grabcartoons/modules/mother_goose.pl
@@ -1,12 +1,12 @@
 sub get_url_mother_goose {
-  my $base="http://www.ucomics.com";
-  my $page="$base/mothergooseandgrimm/";
+  my $base="http://www.grimmy.com";
+  my $page="$base/comics.php";
   my $title="Mother Goose & Grimm";
   fetch_url($page)
     or return (undef, $page, $title);
   while (get_line()) {
-    if (m!src=\".*(images\.ucomics\.com/comics/tmmgg/.*\.gif)!i) {
-	return ("http://$1", $page, $title);
+    if (m!src="(http://www\.grimmy\.com/images/MGG_Archive/.*?\.gif)!i) {
+	return ($1, $page, $title);
     }
   }
   $err="Could not find image in $ {title}'s page";
