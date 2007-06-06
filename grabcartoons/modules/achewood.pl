c2a1c1db2a9c594df78b40450f7b27649e5a9391
Benjamin Kuperman <kuperman@users.sourceforge.net>
2007-06-06 14:40:26 +0000

    Updated xkcd.pl and achewood.pl to new page formats.

--- grabcartoons/modules/achewood.pl
+++ grabcartoons/modules/achewood.pl
@@ -1,6 +1,7 @@
 $COMIC{achewood} = {
 		    'Title' => 'Achewood',
 		    'Page' => 'http://www.achewood.com',
-		    'Regex' => qr!img src="(http://m\.assetbar\.com/[^"]*)"!,
+		    'Regex' => qr!img[^<>]*src="(/comic.php\?date=[^"]*)"!,
 		    'NoShowTitle' => 1,
+                    'Prepend' => '{Page}',
                    };
