2fd5df5ec8ca480df8187c1dd6d07eca7c71a76d
Benjamin Kuperman <kuperman@users.sourceforge.net>
2007-06-12 14:46:55 +0000

    Fixed spelling mistake in Title

--- grabcartoons/modules/calvin_and_hobbes.pl
+++ grabcartoons/modules/calvin_and_hobbes.pl
@@ -1,5 +1,5 @@
 $COMIC{calvin_and_hobbes} = {
-			     'Title' => 'Cavin and Hobbes',
+			     'Title' => 'Calvin and Hobbes',
 			     'Page' => 'http://www.ucomics.com/calvinandhobbes/',
 			     'Regex' => qr(src="(http://images\.ucomics\.com/comics/ch\/.*\.(jpg|gif))),
 			    };
