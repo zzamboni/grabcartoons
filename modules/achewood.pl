d209225af85f2a56e8508d70c59223450f79368b
Benjamin Kuperman <kuperman@users.sourceforge.net>
2007-06-28 20:27:00 +0000

    Now fetching 'title' for Achewood strips

--- modules/achewood.pl
+++ modules/achewood.pl
@@ -2,5 +2,6 @@ $COMIC{achewood} = {
 		    'Title' => 'Achewood',
 		    'Page' => 'http://www.achewood.com',
 		    'Regex' => qr!img src="(http://m\.assetbar\.com/[^"]*)"!,
+		    'ExtraImgAttrsRegex' => qr!img src="http://m\.assetbar\.com/[^"]*" (title="[^"]*")!,
 		    'NoShowTitle' => 1,
                    };
