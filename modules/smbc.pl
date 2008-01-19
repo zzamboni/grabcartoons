4e038788505bd92d729376f2b4ed6fc668236fc0
Diego Zamboni <diego@zzamboni.org>
2008-01-19 17:34:56 +0000

    Fixed smbc

--- modules/smbc.pl
+++ modules/smbc.pl
@@ -1,6 +1,6 @@
 $COMIC{smbc} = {
 		Title => 'Saturday Morning Breakfast Cereal',
 		Page => 'http://www.smbc-comics.com/',
-		Regex => qr!alt="comic" src="/(comics.*.gif)"!i,
+		Regex => qr!src="/(comics/.*.gif)"!i,
 		Prepend => '{Page}',
 	       };
