119cf83b0bf439ac33f912447a65d8f0f162c5fd
Benjamin Kuperman <kuperman@users.sourceforge.net>
2008-03-11 13:10:53 +0000

    Updated image search pattern to include 'png'.
    We might want to make a regexp for this that can be used elsewhere.

--- modules/megatokyo.pl
+++ modules/megatokyo.pl
@@ -1,7 +1,7 @@
 $COMIC{megatokyo} = {
 		     Title => 'MegaTokyo',
 		     Page => 'http://www.megatokyo.com/',
-		     Regex => qr!IMG[^>]*SRC="\/?(strips\/\d+\.(gif|jpg))"!i,
+		     Regex => qr!IMG[^>]*SRC="\/?(strips\/\d+\.(gif|jpg|png))"!i,
 		     ExtraImgAttrsRegex => qr!IMG[^>]*SRC=[^>]*strips[^>]*(title="[^"]*")!i,
 		     Prepend => '{Page}',
 		    };
