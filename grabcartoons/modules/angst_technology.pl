80587b7915ee12782f79ee38084eab2b1be0ca61
Diego Zamboni <diego@zzamboni.org>
2008-04-24 23:29:38 +0000

    * modules/pVp.pl,modules/toothpastefordinner.pl: Fixed for changes 	in the web pages. 	* modules/angst_technology.pl: Removed this module, since the 	comic is no longer being published. 	* modules/concerned.pl: Removed this module, since the 	comic is no longer being published.

--- grabcartoons/modules/angst_technology.pl
+++
@@ -1,8 +0,0 @@
-$COMIC{angst_technology} = {
-			    'Title' => 'Angst Technology',
-			    'Base' => 'http://inktank.com',
-			    'Page' => '{Base}/AT/index.cfm',
-			    'Regex' => qr/SRC="(\/images\/AT\/cartoons\/\d\d-\d\d-\d\d\.gif)"/,
-			    'Prepend' => '{Base}',
-			    'NoShowTitle' => 1,
-                           };
--- grabcartoons/modules/toothpastefordinner.pl
+++ grabcartoons/modules/toothpastefordinner.pl
@@ -1,5 +1,6 @@
 $COMIC{toothpastefordinner} = {
 			       Title => 'Toothpaste for Dinner',
 			       Page => 'http://www.toothpastefordinner.com/',
-			       Regex => qr(Today's comic: \<a href="(http://www.toothpastefordinner.com/\d+/\S+.gif)"),
+			       Regex => qr(Today on Toothpaste For Dinner: \<a href="(http://www.toothpastefordinner.com/\S+.gif)")i,
+			       TitleRegex => qr(Today on Toothpaste For Dinner: \<a href=".*?">(.*?)\</b)i,
 			      };
