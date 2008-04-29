877b0e89726b9ad02928991fc0fb1c0e869d5998
Diego Zamboni <diego@zzamboni.org>
2008-04-29 10:59:04 +0000

    * modules/phd.pl: Added TitleRegex field for capturing the comic's title.

--- modules/phd.pl
+++ modules/phd.pl
@@ -3,5 +3,6 @@ $COMIC{phd} = {
 	       Base => 'http://www.phdcomics.com',
 	       Page => 'http://www.phdcomics.com/comics.php',
 	       Regex => qr/src=(?:http:\/\/www.phdcomic(?:s)?.com\/)?(comics\/archive\/[^.]+\.gif)/,
+	       TitleRegex => qr!<title>PHD Comics:\s+(.+)</title>!i,
 	       Prepend => '{Base}/',
 	      };
