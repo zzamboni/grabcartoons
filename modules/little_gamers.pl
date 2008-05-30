6a145ce56332569fc936bb2b74caba68ad8377cd
Benjamin Kuperman <kuperman@users.sourceforge.net>
2008-05-30 15:02:06 +0000

    Fixed Little Gamers for their new site layout.  They are still working on it, so more changes may be forthcoming.

--- modules/little_gamers.pl
+++ modules/little_gamers.pl
@@ -1,6 +1,6 @@
 $COMIC{little_gamers} = {
 			 Title => 'Little Gamers',
 			 Page => 'http://www.little-gamers.com/',
-			 Regex => qr/img src="(http:\/\/(pimp|www).little-gamers.com\/comics\/\d+.(gif|jpg))" alt=".*" border="0"/i,
+			 Regex => qr/img src="(http:\/\/(pimp|www).little-gamers.com\/comics\/\d+(-\d+)*.(gif|jpg))" alt=".*"/i,
 			 Prepend => '',
 			};
