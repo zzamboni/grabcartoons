3c58c1f11dc13061177cbda9b05c0484815268e9
Benjamin Kuperman <kuperman@users.sourceforge.net>
2008-11-09 18:26:04 +0000

    Updated for new page/URL

--- grabcartoons/modules/rose_is_rose.pl
+++ grabcartoons/modules/rose_is_rose.pl
@@ -1,7 +1,7 @@
 $COMIC{rose_is_rose} = {
 			Title => 'Rose is Rose',
-			Base => 'http://www.unitedmedia.com',
-			Page => '{Base}/comics/roseisrose/',
-			Regex => qr/SRC\s*=\s*"(.*?images\/roseisrose.*?\.(gif|jpg))".*Today's Comic/i,
+			Base => 'http://comics.com',
+			Page => '{Base}/rose_is_rose/',
+			Regex => qr/src\s*=\s*"(.*?dyn\/str_strip\/.*?\.full\.(gif|jpg))"/i,
 			Prepend => '{Base}',
 		       };
