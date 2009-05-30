f1c686497a26171744d7f3fe06cce5aa6c1cdb7e
Diego Zamboni <diego@zzamboni.org>
2009-05-30 23:18:47 +0200

    Fixed for change in the webpage.

--- grabcartoons/modules/toothpastefordinner.pl
+++ grabcartoons/modules/toothpastefordinner.pl
@@ -1,6 +1,6 @@
 $COMIC{toothpastefordinner} = {
 			       Title => 'Toothpaste for Dinner',
 			       Page => 'http://www.toothpastefordinner.com/',
-			       Regex => qr(Today on Toothpaste For Dinner: \<a href="(http://www.toothpastefordinner.com/\S+.gif)")i,
+			       Regex => qr(Today on Toothpaste For Dinner:\s*\<a href="(http://www.toothpastefordinner.com/\S+.gif)")i,
 			       TitleRegex => qr(Today on Toothpaste For Dinner: \<a href=".*?">(.*?)\</b)i,
 			      };
