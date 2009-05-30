b4aa18a9e601dbf1e50f664892b004e4cbf4e23a
Diego Zamboni <diego@zzamboni.org>
2009-05-30 23:14:39 +0200

    Fixed for change in web page, and added TitleRegex

--- grabcartoons/modules/pVp.pl
+++ grabcartoons/modules/pVp.pl
@@ -1,5 +1,6 @@
 $COMIC{pvp} = {
 	       Title => 'pVp',
 	       Page => 'http://www.pvponline.com/',
-	       Regex => qr!src='({Page}comics/pvp\d+\.gif)'!,
+	       Regex => qr!src=['"]({Page}comics/pvp\d+\.gif)['"]!,
+	       TitleRegex => qr!src=['"]{Page}comics/pvp\d+\.gif['"].*title="(.*)"!,
 	      };
