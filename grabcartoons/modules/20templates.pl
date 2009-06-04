3689ff7fe82bc43752aca5f0a53ab883b0602a85
Diego Zamboni <diego@zzamboni.org>
2009-06-05 01:13:38 +0200

    Added _Template_Init to the comics.com_{small,big} templates. The function gets the list of existing comics from the web site, and stores it locally. This makes it possible for _Template_Code (also modified) to check if the comic exists, and to get the proper title for each comic, without having to rely on generic name-transformation rules to get its "short name".

--- grabcartoons/modules/20templates.pl
+++ grabcartoons/modules/20templates.pl
@@ -1,21 +1,89 @@
 # Templates for comics.com strips, in both large and small formats.
-
 $TEMPLATE{'comics.com_big'} = {
+    '_Template_Name' => 'comics.com_big',
     '_Template_Description' => "Comics hosted at comics.com, big version",
     'Base' => 'http://comics.com',
     'Page' => '{Base}/{Tag}/',
     'Regex' => qr(a href="(http://assets\.comics\.com/dyn/str_strip/.*?\.zoom\.(gif|png|jpg))"),
-    '_Template_Code' => sub { $H=shift; unless ($H->{Tag}) { my $tag=lc($H->{Title}); $tag=~s/\s/_/g; $H->{Tag} = $tag } },
+    '_Init_Code' => sub { $H=shift;
+      vmsg("  Initializing template '$H->{_Template_Name}'\n");
+      # Get the list of comics from the website
+      die "Error fetching $H->{Base} to get list of comics\n" unless fetch_url($H->{Base});
+      while (get_line()) {
+	if (/CMC\.Comics\s+=\s+(.*);\s*$/) {
+	  $comics=$1;
+	  # $comics is a Javascript data structure - convert it to Perl, store it in the hash and sanitize it a bit
+	  $comics =~ s/"Description":".*?","/"/g;
+	  $comics =~ s/":/"=>/g;
+	  $comics =~ s/\[\{/{/g;
+	  $comics =~ s/\}\]/}/g;
+	  eval '$H->{_Comics}='.$comics;
+	  die "Internal error in template '$H->{_Template_Name}': $@\n" if $@;
+	  $ch=$H->{_Comics};
+	  foreach $k (keys(%$ch)) {
+	    # We don't need the long descriptions
+	    delete($ch->{$k}->{Description});
+	    # Replace the ID keys with the comic's "tag"
+	    $ch->{$ch->{$k}->{URL}}=$ch->{$k};
+	    delete($ch->{$k});
+	  }
+	  vmsg("    Got comics list from comics.com\n");
+	  return;
+	}
+      }
+      die "Could not find the list of comics for template '$H->{_Template_Name}' in $H->{Base}\n";
+    },
+    '_Template_Code' => sub { $H=shift;
+      # Make sure we have a valid tag
+      $ch=$H->{_Comics};
+      # If a tag was manually set, respect it
+      unless ($H->{Tag}) {
+	# Otherwise, try to derive it from the comic's title
+	my $title=lc($H->{Title});
+	vmsg("    Trying to find the tag for '$title'\n");
+	# First, see if the title is a valid tag already.
+	if ($ch->{$title}) {
+	  # If so, store it as tag and replace the title with the appropriate comic name
+	  $H->{Tag}=$title;
+	  $H->{Title}=$ch->{$title}->{Comic};
+	  vmsg("      Done - comic title was the tag\n");
+	}
+	else {
+	  # Second, search for the title in the comics we know
+	  vmsg("      Looking for the comic's title '$title' in the list of comics\n");
+	  my $tag=(grep { lc($ch->{$_}->{Comic}) eq $title } keys(%$ch))[0];
+	  # If that doesn't work, try some blind normalization
+	  if ($tag) {
+	    vmsg("      Found the title in the list of comics\n");
+	  }
+	  else {
+	    vmsg("      Title not found in comics list - trying some normalization\n");
+	    $tag=$title;
+	    $tag=~s/\s+\&\s+/&/g;
+	    $tag=~s/\.//g;
+	    $tag=~s/'//g;
+	    $tag=~s/\s/_/g;
+	  }
+	  $H->{Tag} = $tag;
+	  # Set the proper title if we can
+	  if (exists($ch->{$tag})) {
+	    $H->{Title} = $ch->{$tag}->{Comic};
+	  }
+	}
+      }
+      # No matter how we got the tag, check that it is valid
+      die "Error: Could not find comic '$H->{Tag}' in template '$H->{_Template_Name}'\n" unless exists($ch->{$H->{Tag}});
+    }
 };
 
-$TEMPLATE{'comics.com_small'} = {
-    '_Template_Description' => "Comics hosted at comics.com, smaller version",
-    'Base' => 'http://comics.com',
-    'Page' => '{Base}/{Tag}/',
-    'Regex' => qr(img src="(http://assets\.comics\.com/dyn/str_strip/.*?\.full\.(gif|png|jpg))"),
-    '_Template_Code' => sub { $H=shift; unless ($H->{Tag}) { my $tag=lc($H->{Title}); $tag=~s/\s/_/g; $H->{Tag} = $tag } },
-};
+# This is the same, we just change the description and the regex
+my %tmp=%{$TEMPLATE{'comics.com_big'}}; # Need the temp var to duplicate the hash and not just copy the hash ref
+$TEMPLATE{'comics.com_small'} = \%tmp;
+$TEMPLATE{'comics.com_small'}->{'_Template_Name'} = 'comics.com_small';
+$TEMPLATE{'comics.com_small'}->{'_Template_Description'} = "Comics hosted at comics.com, smaller version";
+$TEMPLATE{'comics.com_small'}->{'Regex'} = qr(img src="(http://assets\.comics\.com/dyn/str_strip/.*?\.full\.(gif|png|jpg))");
 
+# Template for gocomics.com
 $TEMPLATE{'gocomics.com'} = {
     '_Template_Description' => "Comics hosted at gocomics.com",
     'Base' => 'http://www.gocomics.com',
