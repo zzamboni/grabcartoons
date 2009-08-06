de6868e3c86920d23925df0c8e98bba255020cbc
kuperman <kuperman@acm.org>
2009-08-06 08:50:00 -0400

    Slight modification for a new webpage layout

--- grabcartoons/modules/sluggy_freelance.pl
+++ grabcartoons/modules/sluggy_freelance.pl
@@ -2,4 +2,5 @@ $COMIC{sluggy_freelance} = {
 			    Title => 'Sluggy Freelance',
 			    Page => 'http://www.sluggy.com/',
 			    Regex => qr/SRC\s*=\s*"(.*\/comics\/.*?\.(gif|jpg))"/i,
+                            Prepend => '{Page}',
 			   };
