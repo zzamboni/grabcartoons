86ce3585a8d031cefbd022e986ae667459db25ee
kuperman <kuperman@acm.org>
2010-01-11 16:01:55 -0500

    Created a new module for "Unshelved" (webcomic about librarians)

---
+++ grabcartoons/modules/unshelved.pl
@@ -0,0 +1,5 @@
+$COMIC{unshelved} = {
+		 Title => 'Unshelved',
+		 Page => 'http://www.unshelved.com',
+                 Regex => qr!<img[^>]*src="(http://www.unshelved.com/strips/\d+.(gif|png|jpg|jpeg))"!i,
+		};
