40309318cda197bb394fb13861c915bfa5d6b820
Diego Zamboni <diego@zzamboni.org>
2010-08-13 12:44:29 -0500

    Fixed Savage Chickens module, was fetching unwanted content from the page

--- modules/savage_chickens.pl
+++ modules/savage_chickens.pl
@@ -2,7 +2,7 @@ $COMIC{savage_chickens} = {
     Title => 'Savage Chickens',
     Page => 'http://www.savagechickens.com/',
     NoShowTitle => 1,
-    StartRegex => qr(img src="http://(www.)?savagechickens.com/images/)i,
+    StartRegex => qr(img src="http://(www.)?savagechickens.com/images/.*alt="Savage Chickens -)i,
     EndRegex => qr(div class="postmeta"),
     InclusiveCapture => 1,
     SubstOnRegexResult => [ [ qr(\<div class="postmeta"\>), ""],
