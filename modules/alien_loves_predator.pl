cdb0aad30f10c0d0c2d1050d41e255915172b7bf
Diego Zamboni <diego@zzamboni.org>
2011-05-19 00:59:51 -0500

    Modified to use LinkRelImageSrc

--- modules/alien_loves_predator.pl
+++ modules/alien_loves_predator.pl
@@ -2,7 +2,7 @@ $COMIC{alien_loves_predator} = {
 				'Title' => 'Alien Loves Predator',
                                 #'Regex' => qr/src="(.*?strips.*?\.(jpg|gif|png|jpeg))"/,
                                 #'Prepend' => '{Base}',
-				'Regex' => qr/src="(.*?flickr\.com.*?_b\.(jpg|gif|png|jpeg))"/,
+				'LinkRelImageSrc' => 1,
 				'Base' => 'http://alienlovespredator.com',
                                 'Page' => '{Base}/',
                                };
