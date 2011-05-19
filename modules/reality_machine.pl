82d9fe16eefeca8ffee11617eb749c73047f54c2
Diego Zamboni <diego@zzamboni.org>
2011-05-19 01:19:27 -0500

    Removed reality_machine module, comic is no longer published.

--- modules/reality_machine.pl
+++
@@ -1,5 +0,0 @@
-$COMIC{reality_machine} = {
-    Title => 'Reality Machine',
-    Page => 'http://realitymachine.net',
-    Regex => qr!<img src="(http://realitymachine.net/strips/sog.*.png)"!i,
-};
