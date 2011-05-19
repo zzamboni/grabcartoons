52a1e7c69a1b887d1947c864e177e99bea273a3e
Diego Zamboni <diego@zzamboni.org>
2011-05-19 01:14:37 -0500

    Fixed Mother Goose module for changes in the page.

--- modules/mother_goose.pl
+++ modules/mother_goose.pl
@@ -2,5 +2,6 @@ $COMIC{mother_goose} = {
 			Title => 'Mother Goose &amp; Grimm',
 			Base => 'http://www.grimmy.com',
 			Page => '{Base}/comics.php',
-			Regex => qr!src="(http://www\.grimmy\.com/images/MGG_Archive/.*?\.gif)!i,
+			Regex => qr!swapDaily\(\'(images/MGG_Archive/.*?\.gif)!i,
+			Prepend => '{Base}/',
 		       };
