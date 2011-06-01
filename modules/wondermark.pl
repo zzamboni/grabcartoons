63f9ae129fa1c6d9d5ce417a0f76f56e94a2ade6
Diego Zamboni <diego@zzamboni.org>
2011-06-01 01:28:58 -0500

    Fixed for changes in the page

--- modules/wondermark.pl
+++ modules/wondermark.pl
@@ -1,7 +1,8 @@
 $COMIC{wondermark} = {
 		      Title => 'Wondermark',
 		      Page => 'http://wondermark.com/',
-		      Regex => qr!src="({Page}c/.*\.gif)"!i,
-		      ExtraImgAttrsRegex => qr!src="{Page}c/.*\.gif".*(alt=.*title=".*?")!i,
+		      Regex => qr!src="/(c/.*\.gif)"!i,
+		      Prepend => '{Page}',
+		      ExtraImgAttrsRegex => qr!src="/c/.*\.gif".*(alt=.*title=".*?")!i,
 		      NoShowTitle => 1,
 		     };
