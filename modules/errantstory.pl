2f7697f02a88a12798a70748d23d4ffae191f40f
Diego Zamboni <diego@zzamboni.org>
2011-08-26 01:37:39 -0500

    Added module for http://www.errantstory.com/

---
+++ modules/errantstory.pl
@@ -0,0 +1,7 @@
+$COMIC{errantstory} = {
+		Title => 'Errant Story',
+		Page => 'http://www.errantstory.com/',
+		Regex => qr!src="({Page}comics/.*?)"!i,
+		TitleRegex => qr!src="{Page}comics.*?".*title="(.*?)"!i,
+		ExtraImgAttrsRegex => qr!src="{Page}comics.*?".*(title=".*?")!i,
+	       };
