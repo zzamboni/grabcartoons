425ddf0c660514f602e561ebc187f168d94eae35
Diego Zamboni <diego@zzamboni.org>
2011-08-26 01:42:45 -0500

    Removed eightbit.pl module because comic is no longer being updated.

--- modules/eightbit.pl
+++
@@ -1,7 +0,0 @@
-$COMIC{eightbit} = {
-			     Title => '8-Bit Theater',
-			     Base => 'http://www.nuklearpower.com/',
-			     Page => '{Base}8-bit-theater/',
-			     Regex => qr(src="(http://.*(static|www|cdn)\.nuklearpower\.com/comics/8-bit-theater/\d+\.(gif|jpg|png))"),
-			     TitleRegex => qr(\<div class="navbar-title"\>(.*?)\</div\>),
-			    };
