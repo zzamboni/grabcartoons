bcc8a2948cc4b5c914c128d09de1d00a5b6f920a
Diego Zamboni <diego@zzamboni.org>
2012-02-28 15:58:28 -0600

    Added module for http://www.danscartoons.com/, contributed by @QustionsMarcus. Closes issue #6.

---
+++ modules/danscartoons.pl
@@ -0,0 +1,5 @@
+$COMIC{danscartoons} = {
+    'Title' => "Dan's Daily Cartoon",
+    'Page'  => 'http://www.danscartoons.com/online_dailycartoon_content.htm',
+    'Regex' => qr!src="(http://danscartoons.com/cotd/dailycartoon.php)"!i,
+};
