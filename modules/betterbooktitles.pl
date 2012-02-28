0b194a5c75fcca06d25fc0dfcdf1b9f28ffb5b27
Diego Zamboni <diego@zzamboni.org>
2012-02-28 16:09:30 -0600

    * modules/betterbooktitles.pl: Added module for http://betterbooktitles.com/, contributed by @QustionsMarcus. Closes #4.

---
+++ modules/betterbooktitles.pl
@@ -0,0 +1,5 @@
+$COMIC{betterbooktitles} = {
+    'Title' => 'Better Book Titles',
+    'Page'  => 'http://betterbooktitles.com',
+    'Regex' => qr!src="(http://\d+.media.tumblr.com/tumblr_.*.jpg)"!i,
+};
