bd0e2c1be1a541f8a97cec4fe4ff5094ec2880f4
Diego Zamboni <diego@zzamboni.org>
2012-06-25 15:42:55 -0500

    Updated module for Savage Chickens to use the gocomics.com template.

--- modules/savage_chickens.pl
+++ modules/savage_chickens.pl
@@ -1,8 +1,4 @@
 $COMIC{savage_chickens} = {
     Title => 'Savage Chickens',
-    Page => 'http://www.savagechickens.com/',
-    NoShowTitle => 1,
-    StartRegex => qr(div class="entry_content")i,
-    EndRegex => qr(div class="clear"),
-    InclusiveCapture => 0,
+    Template => 'gocomics.com',
 }
