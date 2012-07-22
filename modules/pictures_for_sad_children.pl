5ca25da96f638be9845731360d66e33a5c9fd42d
kuperman <kuperman@acm.org>
2012-07-22 15:06:44 -0400

    Update pictures_for_sad_children.pl
    Updates module to support the images being hosted on tumblr and being of
    various types.

--- modules/pictures_for_sad_children.pl
+++ modules/pictures_for_sad_children.pl
@@ -1,7 +1,9 @@
 $COMIC{pictures_for_sad_children} = {
 		    'Title' => 'Pictures for Sad Children',
 		    'Page' => 'http://www.picturesforsadchildren.com',
-		    'Regex' => qr!img src="(http://www\.picturesforsadchildren\.com/comics/[^"]*)"!,
-		    'ExtraImgAttrsRegex' => qr!img src="http://www\.picturesforsadchildren\.com/comics/[^"]*" (title="[^"]*")!,
+		    'Regex' => qr!class="photo-wrapper".*img src="(http://.*\.media\.tumblr\.com/[^"]*_[0-9]*.(?:png|gif|jpg))"!,
+		    'ExtraImgAttrsRegex' => qr!class="photo-wrapper".*img src="http://.*\.media\.tumblr\.com/[^"]*_[0-9]*.(?:png|gif|jpg)" (title="[^"]*")!,
+                    #'Regex' => qr!img src="(http://www\.picturesforsadchildren\.com/comics/[^"]*)"!,
+                    #'ExtraImgAttrsRegex' => qr!img src="http://www\.picturesforsadchildren\.com/comics/[^"]*" (title="[^"]*")!,
 		    'NoShowTitle' => 0,
                    };
