2f96f950a6a533171737fbbe6403e0f0d14b53c6
Benjamin Kuperman <kuperman@acm.org>
2012-10-01 10:33:53 -0400

    Minor tweak for new img URL style.

--- modules/little_gamers.pl
+++ modules/little_gamers.pl
@@ -1,7 +1,7 @@
 $COMIC{little_gamers} = {
 			 Title => 'Little Gamers',
 			 Page => 'http://www.little-gamers.com/',
-			 Regex => qr/img src="(http:\/\/((pimp|www|upload)\.)?little-gamers.(com|eoo.se)\/comics\/\d+(-\d+)*.(gif|jpg))" alt=".*"/i,
-                         'ExtraImgAttrsRegex' => qr!img src="[^"]*/comics/\d+[^"]*" alt="[^"]*" (title="[^"]*")!,
+			 Regex => qr/img[^>]*src="(http:\/\/((pimp|www|upload)\.)?little-gamers.(com|eoo.se)\/comics\/\d+(-\d+)*.(gif|jpg|jpeg|png))" alt=".*"/i,
+                         'ExtraImgAttrsRegex' => qr!img[^>]*src="[^"]*/comics/\d+[^"]*" alt="[^"]*" (title="[^"]*")!,
 			 Prepend => '',
 			};
