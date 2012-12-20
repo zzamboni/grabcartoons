cefb446059c57dea2ce64e120fef5a2389746a80
Benjamin Kuperman <kuperman@acm.org>
2012-12-20 11:05:31 -0500

    Minor tweaks to regexps

--- modules/girls_with_slingshots.pl
+++ modules/girls_with_slingshots.pl
@@ -3,7 +3,7 @@ $COMIC{girls_with_slingshots} = {
     'Title' => 'Girls With Slingshots',
     #'StartRegex' => qr/div id="comic"/,
     #'EndRegex' => qr(\</div\>),
-    'Regex' => qr(src="(http://cdn.*?/comics/.*?)"),
+    'Regex' => qr!src="(http://cdn.*?/comics/.*?)"!i,
     'ExtraImgAttrsRegex' => qr/(title="[^"]*")/,
     'Base' => 'http://www.girlswithslingshots.com',
     'Page' => '{Base}/',
--- modules/three_panel_soul.pl
+++ modules/three_panel_soul.pl
@@ -2,5 +2,5 @@ $COMIC{three_panel_soul} = {
 			Title => 'Three Panel Soul',
 			Page => 'http://threepanelsoul.com/',
 			Regex => qr!src="(http://(?:www.)?threepanelsoul.com/comics/[-0-9]+.*\.(?:png|jpg|jpeg|gif))"!i,
-                        ExtraImgAttrsRegex => qr!src=".*/comics/.*\.(?:png|jpg|jpeg|gif)".*(alt=.*title=".*?")!i,
+                        ExtraImgAttrsRegex => qr!(alt=.*title=".*?")!i,
 		       };
