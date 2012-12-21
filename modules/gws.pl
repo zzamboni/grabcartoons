67351abb5945e48f6804f32a6d00407d70799631
Benjamin Kuperman <kuperman@acm.org>
2012-12-21 10:56:11 -0500

    Updating GWS for new website layout

--- modules/gws.pl
+++ modules/gws.pl
@@ -1,6 +1,6 @@
 $COMIC{gws} = {
 		Title => 'Girls with Slingshots',
 		Page => 'http://www.girlswithslingshots.com/',
-		Regex => qr!src="({Page}comics/.*?)"!i,
-		ExtraImgAttrsRegex => qr!src="{Page}comics.*?".*(title=".*?")!i,
+		Regex => qr!src="(http://cdn.*?/comics/.*?)"!i,
+		ExtraImgAttrsRegex => qr!src="http://cdn.*?/comics/.*?".*?(title=".*?")!i,
 	       };
