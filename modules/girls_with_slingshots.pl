53be28b26b1374d9c9315946f31b2fed3cb85bb9
Benjamin Kuperman <kuperman@acm.org>
2012-12-21 10:54:42 -0500

    Undoing addition of duplicate module for GWS

--- modules/girls_with_slingshots.pl
+++
@@ -1,6 +0,0 @@
-$COMIC{girls_with_slingshots} = {
-        Title => 'Girls with Slingshots',
-        Page => 'http://www.girlswithslingshots.com/',
-        Regex => qr!src="(http://cdn.*?/comics/.*?)"!i,
-        ExtraImgAttrsRegex => qr/(title="[^"]*")/,
-    };
