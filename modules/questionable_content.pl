45996afafc288969bf9b6bf72556875e6ca04ac7
Benjamin Kuperman <kuperman@acm.org>
2013-01-09 15:43:56 -0500

    minor tweaks for image urls

--- modules/questionable_content.pl
+++ modules/questionable_content.pl
@@ -1,5 +1,5 @@
 $COMIC{questionable_content} = {
 			Title => 'Questionable Content',
 			Page => 'http://questionablecontent.net/',
-			Regex => qr!src="(http://www.questionablecontent.net/comics/.*\.(?:png|gif|jpg|jpeg))"!i,
+			Regex => qr!src="(http://www.questionablecontent.net/comics/.*\.png)"!i,
 		       };
