9525f578eccc00e74cfd150bcc3bb597b696be17
Benjamin Kuperman <kuperman@acm.org>
2013-01-09 15:45:54 -0500

    change indentation

--- modules/three_panel_soul.pl
+++ modules/three_panel_soul.pl
@@ -1,6 +1,6 @@
 $COMIC{three_panel_soul} = {
-			Title => 'Three Panel Soul',
-			Page => 'http://threepanelsoul.com/',
-			Regex => qr!src="(http://(?:www.)?threepanelsoul.com/comics/[-0-9]+.*\.(?:png|jpg|jpeg|gif))"!i,
-                        ExtraImgAttrsRegex => qr!(alt=.*title=".*?")!i,
-		       };
+            Title => 'Three Panel Soul',
+            Page => 'http://threepanelsoul.com/',
+            Regex => qr!src="(http://(?:www.)?threepanelsoul.com/comics/[-0-9]+.*\.(?:png|jpg|jpeg|gif))"!i,
+            ExtraImgAttrsRegex => qr!(alt=.*title=".*?")!i,
+        };
