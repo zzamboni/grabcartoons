07dca8e75de294baa22eafc47130990ff40ca1e4
Benjamin Kuperman <kuperman@acm.org>
2013-02-18 09:57:55 -0500

    Updates to "Joy of Tech"

--- modules/joy_of_tech.pl
+++ modules/joy_of_tech.pl
@@ -1,6 +1,6 @@
 $COMIC{joy_of_tech} = {
 		       Title => 'The Joy of Tech',
-		       Page => 'http://www.geekculture.com/joyoftech/',
+		       Page => 'http://www.joyoftech.com/joyoftech/',
 		       Regex => qr/src="(joyimages\/\d+([a-z])?\.(?:gif|png|jpg))"/,
 		       Prepend => '{Page}',
 		       NoShowTitle => 1,
