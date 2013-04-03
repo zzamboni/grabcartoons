cd82d051283df53cb372f6ff73fe6c8198c85070
Benjamin Kuperman <kuperman@acm.org>
2013-04-03 12:03:47 -0400

    Fix pVp image regexp

--- modules/pVp.pl
+++ modules/pVp.pl
@@ -2,6 +2,6 @@ $COMIC{pvp} = {
 	       Title => 'pVp',
 	       Base => 'http://www.pvponline.com/',
                Page => '{Base}comic',
-               Regex => qr!src="(.*/img/comic/pvp\d+\.jpg)"!,
+               Regex => qr!src="(.*/img/comic/\d+/\d+/pvp\d+\.jpg)"!,
                TitleRegex => qr!<h2 id="headingArchive">(.*)</h2>!i,
 	      };
