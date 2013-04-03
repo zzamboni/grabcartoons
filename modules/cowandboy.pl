370669e2865fe7854d52e46fdbad970b51881164
Benjamin Kuperman <kuperman@acm.org>
2013-04-03 11:19:15 -0400

    fix for possibly new layout

--- modules/cowandboy.pl
+++ modules/cowandboy.pl
@@ -1,8 +1,8 @@
 $COMIC{cowandboy} = {
     Title => 'Cow and Boy',
     Page => 'http://www.cowandboy.com/',
-    Regex => qr!<img src="(.*\.gif)".*title=".*">!i,
-    TitleRegex => qr!img[^<>]*title="(.*)"!i,
-    ExtraImgAttrsRegex => qr!img[^<>]*(alt=".*")!i,
+    Regex => qr!<img src="(cb\d*\.gif)"(?:[^>]*title=".*">)?!i,
+    #TitleRegex => qr!img[^<>]*title="(.*)"!i,
+    #ExtraImgAttrsRegex => qr!img[^<>]*(alt=".*")!i,
     Prepend => '{Page}',
 }
