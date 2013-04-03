cf061e8ee4bab26c317036584d1ae220c9772814
Benjamin Kuperman <kuperman@acm.org>
2013-04-03 11:40:48 -0400

    Fix 'Extra Ordinary' for new URL/layout

--- modules/extraordinary.pl
+++ modules/extraordinary.pl
@@ -1,6 +1,12 @@
 $COMIC{extraordinary} = {
     Title => 'Extra Ordinary',
-    Page => 'http://exocomics.com/',
-    Regex => qr!img +src="(.+?)".*class="comic-item!i,
-    ExtraImgAttrsRegex => qr!(title=".*?")!i,
+    Page => 'http://www.exocomics.com/',
+    StartRegex => qr!<a id="comic-\d+" class="comic">!,
+    EndRegex => qr!</a>!,
+    InclusiveCapture => 0,
+    SubstOnRegexResult => [ 
+        [ qr(class="[^"]*"), '', 1 ],
+        [ qr(alt="[^"]*"), '', 1 ],
+        [ qr([\s_]+), ' ', 1 ],
+    ],
 };
