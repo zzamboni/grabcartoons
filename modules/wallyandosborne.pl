9f9c4ab111585d19923a75f4c9b0583571c69bba
Benjamin Kuperman <kuperman@acm.org>
2013-04-03 12:17:00 -0400

    Fix 'Wally and Osborne' image regexp

--- modules/wallyandosborne.pl
+++ modules/wallyandosborne.pl
@@ -1,5 +1,6 @@
 $COMIC{wallyandosborne} = {
     Title => 'Wally and Osborne',
     Page => 'http://wallyandosborne.com/',
-    Regex => qr!<img src=['"](http://wallyandosborne.com[/]+comics/\S+)["']!i,
+    Regex => qr!<img src=['"](http://wallyandosborne.com\/[^"']*/\d+-\d+-\d+-\d+\.(?:gif|png|jpg|jpeg))["']!i,
+    ExtraImgAttrsRegex => qr!(title=".*")!,
 };
