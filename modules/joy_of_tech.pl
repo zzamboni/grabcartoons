7bee4e42d1516fc7b88685685fb7139717bb3627
Benjamin Kuperman <kuperman@acm.org>
2013-04-04 08:21:45 -0400

    drop page prepend from Joy of Tech

--- modules/joy_of_tech.pl
+++ modules/joy_of_tech.pl
@@ -2,6 +2,5 @@ $COMIC{joy_of_tech} = {
 		       Title => 'The Joy of Tech',
 		       Page => 'http://www.joyoftech.com/joyoftech/',
 		       Regex => qr/src="([^"]*\/cartoons\/[^"]*\.(?:gif|png|jpg))"/,
-		       Prepend => '{Page}',
 		       NoShowTitle => 1,
 		      };
