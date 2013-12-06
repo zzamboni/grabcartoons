1cbb399e3255a4f9901d71832f9088d06bfb4c4b
Benjamin Kuperman <kuperman@acm.org>
2013-12-05 22:25:40 -0500

    Updated Goblins

--- modules/goblins.pl
+++ modules/goblins.pl
@@ -1,7 +1,9 @@
 $COMIC{goblins} = {
     Title => 'Goblins',
-    Page => 'http://goblinscomic.com',
-    StartRegex => qr(\<div id="comic"\>),
-    EndRegex => qr(\</div\>),
-    SubstOnRegexResult => [ [ qr(src="/comics), 'src="{Page}/comics', 1 ] ],
+    Page => 'http://www.goblinscomic.org',
+    Regex => qr!img src="(?:http://www.goblinscomic.org)?([^"]*?/comics/[^"]*?(?:gif|png|jpg|jpeg))"!i,
+    Prepend => '{Page}/',
+    #StartRegex => qr(\<div id="comic"\>),
+    #EndRegex => qr(\</div\>),
+    #SubstOnRegexResult => [ [ qr(src="/comics), 'src="{Page}/comics', 1 ] ],
 };
