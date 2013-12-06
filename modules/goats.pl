757c475cc38dd380fb36b5684c17d95ba6e05298
Benjamin Kuperman <kuperman@acm.org>
2013-12-05 21:56:53 -0500

    Updated Goats

--- modules/goats.pl
+++ modules/goats.pl
@@ -1,6 +1,6 @@
 $COMIC{goats} = {
 		 Title => 'Goats',
-		 Page => 'http://www.goats.com',
-		 Regex => qr/IMG SRC="(.*\/comix\/.*\.(gif|png|jpg))" WIDTH=(")?\d+(")? HEIGHT=(")?\d+(")?/i,
-		 Prepend => '{Page}'
+		 Page => 'http://goats.com',
+		 Regex => qr/IMG SRC="(.*?\/xgoats\d+.*?\.(?:gif|png|jpg))"/i,
+                 #Prepend => '{Page}'
 		};
