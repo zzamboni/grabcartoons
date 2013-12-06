ddbbc7e4cd337ec8277d6f60226e0570a86dde4f
Benjamin Kuperman <kuperman@acm.org>
2013-12-05 22:29:28 -0500

    Updated Alien Loves Predator

--- modules/alien_loves_predator.pl
+++ modules/alien_loves_predator.pl
@@ -1,7 +1,7 @@
 $COMIC{alien_loves_predator} = {
 				'Title' => 'Alien Loves Predator',
 				'StartRegex' => qr/div id="comic"/,
-				'Regex' => qr/src="(.*?)"/,
+				'Regex' => qr!src="(.*?/strips/.*?)"!i,
 				'Base' => 'http://alienlovespredator.com',
                                 'Page' => '{Base}/',
                                };
