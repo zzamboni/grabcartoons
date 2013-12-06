a4a1e5b4ffb9c7d650329e2ff338f604ec186b79
Benjamin Kuperman <kuperman@acm.org>
2013-12-05 22:00:14 -0500

    Updated Kevin and Kell

--- modules/kevin_and_kell.pl
+++ modules/kevin_and_kell.pl
@@ -1,6 +1,6 @@
 $COMIC{kevin_and_kell} = {
 			  Title => 'Kevin and Kell',
-			  Page => 'http://www.kevinandkell.com/',
-			  Regex => qr/.*SRC\s*=\s*"(.*\/strips\/.*?\.gif)"/i,
+			  Page => 'http://www.kevinandkell.com',
+			  Regex => qr!SRC\s*=\s*"((?:http://www.kevinandkell.com/)?.*?/strips/.*?\.(?:gif|png|jpg|jpeg))"!i,
 			  Prepend => '{Page}',
 			 };
