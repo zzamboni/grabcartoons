731ba2388028aca336ca69f8911d8ffdd92e8723
Benjamin Kuperman <kuperman@acm.org>
2013-12-05 20:45:45 -0500

    Updated multiverse to new image URL

--- modules/multiverse.pl
+++ modules/multiverse.pl
@@ -1,8 +1,9 @@
 $COMIC{multiverse} = {
 		   Title => 'Scenes From A Multiverse',
 		   Page => 'http://amultiverse.com/',
-		   Regex => qr!class="comicpane".*img\s+src\s*=\s*\"(.*/comics/.*?)"!i,
-                   ExtraImgAttrsRegex => qr!img[^<>]*src="[^<>]*/comics/[^<>]*?" ((alt=".*?")\s*(title=".*"))!i,
+                   #StartRegex => qr!<div id="comic">!i,
+                   Regex => qr!<img\s+src="([^<>]*?)"\s+alt=".*?"\s+title=".*?"!i,
+                   ExtraImgAttrsRegex => qr!img[^<>]*src="[^<>]*?" ((alt=".*?")\s*(title=".*"))!i,
                    #TitleRegex => qr!class="comicpane".*title="(.*?)"!i,
                    #ExtraImgAttrs => qr!class="comicpane".*(alt=".*?")!i,
 		  };
