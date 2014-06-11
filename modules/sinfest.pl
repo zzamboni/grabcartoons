434f67f6acd26ad7de7d168669ca6f259c7e9f7f
Benjamin Kuperman <kuperman@acm.org>
2014-06-11 14:51:43 -0400

    Updated Sinfest image URL

--- modules/sinfest.pl
+++ modules/sinfest.pl
@@ -1,6 +1,6 @@
 $COMIC{sinfest} = {
 		   Title => 'Sinfest',
 		   Page => 'http://sinfest.net/',
-		   Regex => qr!SRC\s*=\s*\".*(/comikaze/comics/\d{4}-\d{2}-\d{2}\.(gif|jpg|png))!i,
+		   Regex => qr!SRC\s*=\s*\".*?(\w+/comics/\d{4}-\d{2}-\d{2}\.(gif|jpg|png))!i,
 		   Prepend => '{Page}',
 		  };
