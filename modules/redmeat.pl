b4dd1cda8a9cc4ffd941055e264fe02a27a0d8b2
Benjamin Kuperman <kuperman@acm.org>
2014-07-01 11:31:59 -0400

    Updated RedMeat

--- modules/redmeat.pl
+++ modules/redmeat.pl
@@ -1,8 +1,8 @@
 $COMIC{redmeat} = {
 		   Title => 'Red Meat',
-		   Page => 'http://www.redmeat.com/redmeat/current/',
-		   Regex => qr/img src="(.*\.gif)" width="\d\d\d" height="\d\d\d" border="0"/,
-		   ExtraImgAttrsRegex => qr/img src=".*\.gif" width="\d\d\d" height="\d\d\d" border="0" (alt=".*?")/,
+		   Page => 'http://www.redmeat.com/max-cannon/FreshMeat',
+		   Regex => qr/img src="(.*\.(?:gif|jpg|jpeg|png))" width="\d\d\d" height="\d\d\d"/,
+		   ExtraImgAttrsRegex => qr/img src=".*\.(?:gif|jpg|jpeg|png)" width="\d\d\d" height="\d\d\d" (alt=".*?")/,
 		   Prepend => '{Page}',
 		   'NoShowTitle' => 1,
 		  };
