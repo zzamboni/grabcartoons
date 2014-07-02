980467bcea0fe28907ee1863a0db3843a6226011
Diego Zamboni <diego@zzamboni.org>
2014-07-02 15:18:20 -0500

    modules/sinfest.pl: Updated for new page format.

--- modules/sinfest.pl
+++ modules/sinfest.pl
@@ -1,6 +1,7 @@
 $COMIC{sinfest} = {
 		   Title => 'Sinfest',
 		   Page => 'http://sinfest.net/',
-		   Regex => qr!SRC\s*=\s*\".*(/comikaze/comics/\d{4}-\d{2}-\d{2}\.(gif|jpg|png))!i,
+		   Regex => qr!src="(btphp/comics/\d{4}-\d{2}-\d{2}\.(gif|jpg|png))!i,
+                   TitleRegex => qr!alt="(.*?)"!,
 		   Prepend => '{Page}',
 		  };
