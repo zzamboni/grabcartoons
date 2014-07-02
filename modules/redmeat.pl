9a1ae8f4a941a9fad597fb52c1ad208cdcad1327
Benjamin Kuperman <kuperman@acm.org>
2014-07-02 10:50:34 -0400

    Dropping the prepend

--- modules/redmeat.pl
+++ modules/redmeat.pl
@@ -3,6 +3,6 @@ $COMIC{redmeat} = {
 		   Page => 'http://www.redmeat.com/max-cannon/FreshMeat',
 		   Regex => qr/img src="(.*\.(?:gif|jpg|jpeg|png))" width="\d\d\d" height="\d\d\d"/,
 		   ExtraImgAttrsRegex => qr/img src=".*\.(?:gif|jpg|jpeg|png)" width="\d\d\d" height="\d\d\d" (alt=".*?")/,
-		   Prepend => '{Page}',
+                   #Prepend => '{Page}',
 		   'NoShowTitle' => 1,
 		  };
