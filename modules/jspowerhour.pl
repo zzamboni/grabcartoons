19aee1ecfed13eda6da91bb7cdaeca61911bae31
Benjamin Kuperman <kuperman@acm.org>
2014-07-14 15:48:59 -0400

    Add function to jspowerhour to grab the annotation

--- modules/jspowerhour.pl
+++ modules/jspowerhour.pl
@@ -2,25 +2,67 @@
 $COMIC{jspowerhour} = {
     Title => 'Junior Scientist Power Hour',
     Page => 'http://www.jspowerhour.com/',
-
-    #Regex => qr!<img[^>]*id="comic-img"[^>]*src="([^"]*)"!,
-    #ExtraImgAttrsRegex => qr!(alt="[^"]*")!,
-
-    StartRegex => qr(\<div id="comic"),
-    EndRegex => qr(\<div id="contact"),
-    InclusiveCapture => 1,
-
-    SubstOnRegexResult => [
-        [ qr([\s]+), ' ', 1 ],     # drop the whitespace
-        [ qr(^.*<img), '<img', 0],  # drop the leading stuff
-        [ qr(/>.*?<div id="description"> <h1>), '/></a><br><h1>'], # and the middle stuff
-        [ qr(</div> <div class="clearfix">.*), '<a>'],  # now the trailing stuff
-        [ qr(h1), 'h3'],  # downsize headers
-        [ qr(class="[^"]*"), '', 1 ],   # clean up any classes
-        [ qr(style="[^"]*"), '', 1 ],   # clean up any styles
-        [ qr(<\s*p\s*>), '<p class="comic">', 1 ],   # clean up any styles
-        [ qr(id="[^"]*"), '', 1 ],      # and any ids
-    ],
-
+    Function => \&get_url_jspowerhour,
 };
+
+
+sub get_url_jspowerhour {
+    my $jsp_base="http://www.jspowerhour.com/";
+    my $jsp_title="Junior Scientist Power Hour";
+    my $jsp_comic = undef;
+
+    fetch_url($jsp_base)
+        or return (undef, $jsp_base, $jsp_title);
+
+    # Get the comic
+    while (get_line()) {
+        if (/id="comic-img".*src="([^\"]*\.(?:gif|png|jpg|jpeg)[^\"]*)"/i) {
+            $jsp_comic="$1";
+            last;
+        }
+    }
+
+    $err="Could not find image in <a href=\"$jsp_base\">$jsp_title"."</a>'s page";
+    return (undef, $jsp_base, $err) unless defined $jsp_comic;
+
+    # Get the commentary/alt text
+    my $commentary="", $comic_title = undef;
+    my $incommentary = 0; # haven't found it yet
+    while (get_line()) {
+        
+        # find the start of the block
+        if (/div id="description"/) {
+            $incommentary = 1; # in section, keep looking
+            next;
+        }
+
+        # keep looping until you find the right div
+        next unless ($incommentary > 0);
+
+        # skip other divs
+        next if (/<div/);
+        if (/<h1><a[^>]*>([^<]*)<\/a><\/h1>/) {
+            $comic_title = $1;
+            $incommentary = 2;
+            next;
+        }
+
+        # skip until we find a title
+        next unless ($incommentary > 1);
+
+        # we have found the title, grab stuff until we hit a closing div
+        last if (/<\/div>/);
+
+        # concatenate it all
+        $commentary .= $_;
+
+    }
+
+    if ($incommentary > 0) {
+        return ("<img src=\"$jsp_comic\"></a><h3>$comic_title</h3><div class=\"annotation\">$commentary</div><a>", $jsp_title, undef);
+    } else {
+	return ("<img SRC=\"$jsp_comic\" alt=\"Today's $jsp_title\"><br>", $jsp_title, undef);
+    }
+}
+
 
