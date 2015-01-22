4bec5dafadf60436929376f56b139a1eb368ebbb
Benjamin Kuperman <kuperman@acm.org>
2015-01-22 11:55:00 -0500

    Update dilbert.pl to use new image URL

--- modules/dilbert.pl
+++ modules/dilbert.pl
@@ -2,6 +2,7 @@ $COMIC{dilbert} = {
 		   'Title' => 'Dilbert',
 		   'Base' => 'http://www.dilbert.com',
 		   'Page' => '{Base}/fast',
-		   'Regex' => qr!SRC="(/dyn/str_strip/[^<>]*strip\.print\.(gif|jpg))[^<>]*"!i,
-		   'Prepend' => '{Base}',
+                   'Regex' => qr!<img[^<>]*\bimg-comic\b[^<>]*src="([^"<>]*)"!i,
+                   #'Regex' => qr!SRC="(/dyn/str_strip/[^<>]*strip\.print\.(gif|jpg))[^<>]*"!i,
+                   #'Prepend' => '{Base}',
                   };
