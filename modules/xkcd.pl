66bef6055666e6bf831bd20479cec8897eb3cc71
Benjamin Kuperman <kuperman@acm.org>
2015-03-31 11:41:19 -0400

    Update of XKCD

--- modules/xkcd.pl
+++ modules/xkcd.pl
@@ -1,7 +1,8 @@
 $COMIC{xkcd} = {
 		Title => 'xkcd',
 		Page => 'http://xkcd.com/',
-		Regex => qr!img[^<>]*src="(http://imgs\.xkcd\.com/comics.*?)"!i,
-		TitleRegex => qr!^<h1>(.+)</h1><br/>!i,
-		ExtraImgAttrsRegex => qr!img[^<>]*src="http://imgs\.xkcd\.com/comics.*?" (title=".*")!i,
+		Regex => qr!img[^<>]*src="(?:http:)?//(imgs\.xkcd\.com/comics.*?)"!i,
+                Prepend => 'http://', 
+		TitleRegex => qr!^<div id="ctitle">(.+)</div>!i,
+		ExtraImgAttrsRegex => qr!(title=".*")!i,
 	       };
