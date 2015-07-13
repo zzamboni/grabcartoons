a2bfba0258df8dd1fe72c96b044bd2ac51eee56b
Benjamin Kuperman <kuperman@acm.org>
2015-07-13 11:18:25 -0400

    Check for ad banner image in Joy of Tech

--- modules/joy_of_tech.pl
+++ modules/joy_of_tech.pl
@@ -1,7 +1,7 @@
 $COMIC{joy_of_tech} = {
     Title => 'The Joy of Tech',
     Page => 'http://www.joyoftech.com/joyoftech/',
-    Regex => qr/src="[^"]*(joyimages\/[^"]*\.(?:gif|png|jpg))"/,
+    Regex => qr/src="[^"]*(joyimages\/[^"]*(?<!banner)\.(?:gif|png|jpg))"/,
     ExtraImgAttrsRegex => qr/(alt="[^"]*")/,
     Prepend => '{Page}',
     NoShowTitle => 1,
