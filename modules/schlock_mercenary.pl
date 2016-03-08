d81c05d87678fbf11e4a4c4141f9cfa25253474f
Benjamin Kuperman <kuperman@acm.org>
2016-03-08 10:30:07 -0500

    Update to Schlock Mercenary

--- modules/schlock_mercenary.pl
+++ modules/schlock_mercenary.pl
@@ -1,8 +1,13 @@
 $COMIC{schlock_mercenary} = {
     Title => 'Schlock Mercenary',
-    Base => 'http://www.schlockmercenary.com/',
+    Base => 'https://www.schlockmercenary.com/',
     Page => '{Base}',
-    StartRegex => qr(\<div id='comic'),
+    InclusiveCapture => 1,
+    StartRegex => qr(\<div class="strip-image-wrapper"),
     EndRegex => qr(\</div\>|\<!--AIO_END--\>),
-    SubstOnRegexResult => [ [ qr(src="/comics), 'src="{Base}comics', 1 ] ],
+    SubstOnRegexResult => [ 
+        [ qr(.*\<div class="strip-image-wrapper">), '', 1 ],
+        [ qr(\<\/div\>.*), '', 1 ],
+        [ qr(src="/strip), 'src="{Base}strip', 1 ],
+    ],
 };
