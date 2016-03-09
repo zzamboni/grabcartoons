54778ea97d0ddd65b2540f6e69553db7554a329b
Benjamin Kuperman <kuperman@acm.org>
2016-03-09 10:37:00 -0500

    Update Penny Arcade

--- modules/penny_arcade.pl
+++ modules/penny_arcade.pl
@@ -3,6 +3,6 @@ $COMIC{penny_arcade} = {
 			Base => 'http://www.penny-arcade.com',
 			Page => '{Base}/comic',
                         #Regex => qr@src="(http://art.penny-arcade.com/photos/[^\"]*?\.(gif|jpg))"@i,
-			Regex => qr@src="(http(s)?://[^/]*penny-arcade[^/]*/photos/[^\"]*?\.(gif|jpg))"@i,
+			Regex => qr@src="(http(s)?://[^/]*penny-arcade[^/]*/Comics/[^\"]*?\.(gif|png|jpeg|jpg))"@i,
                         TitleRegex => qr@<title>Penny Arcade - Comic -\s+(.+)</title>@i,
 		       };
