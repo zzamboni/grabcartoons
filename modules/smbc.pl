263c732ec699455a1b2e93535df9568361cd6d88
Benjamin Kuperman <kuperman@acm.org>
2016-03-14 09:37:47 -0400

    Tweak of SMBC regexp

--- modules/smbc.pl
+++ modules/smbc.pl
@@ -1,7 +1,7 @@
 $COMIC{smbc} = {
 		Title => 'Saturday Morning Breakfast Cereal',
 		Page => 'http://www.smbc-comics.com/',
-		Regex => qr!src=["'].*[/]?(comics/.*\d+.(?:gif|png|jpg|jpeg))["']!i,
+		Regex => qr!src=["'].*[/]?(comics/.*\d+(?:\s*\(\d+\))?.(?:gif|png|jpg|jpeg))["']!i,
                 ExtraImgAttrsRegex => qr!(title=".*?")!i,
 		Prepend => '{Page}',
 	       };
