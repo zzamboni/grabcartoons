210a2c768a7f4d4b3f4b690acb50dac1cee1b86a
Benjamin Kuperman <kuperman@acm.org>
2016-03-22 15:13:44 -0400

    Tweak multiverse.pl to handle embedded newlines

--- modules/multiverse.pl
+++ modules/multiverse.pl
@@ -1,9 +1,16 @@
 $COMIC{multiverse} = {
 		   Title => 'Scenes From A Multiverse',
 		   Page => 'http://amultiverse.com/',
-                   #StartRegex => qr!<div id="comic">!i,
-                   Regex => qr!<img\s+src="([^<>]*?)"\s+alt=".*?"\s+title=".*?"!i,
-                   ExtraImgAttrsRegex => qr!img[^<>]*src="[^<>]*?" ((alt=".*?")\s*(title=".*"))!i,
+                   StartRegex => qr!<div id="comic">!i,
+                   EndRegex => qr!</div>!i,
+                   InclusiveCapture => True,
+                   SubstOnRegexResult => [
+                       [ qr!\s*<div id="comic">\s*!, '' ],
+                       [ qr((\R)+), '', 1 ],
+                       [ qr!\s*</div>\s*!, '' ],
+                   ],
+                   #Regex => qr!img\s+src\s*=\s*"(.*?(?:gif|png|jpg|jpeg))"!i,
+                   #ExtraImgAttrsRegex => qr!((alt=".*?")\s*(title=".*"))!i,
                    #TitleRegex => qr!class="comicpane".*title="(.*?)"!i,
                    #ExtraImgAttrs => qr!class="comicpane".*(alt=".*?")!i,
 		  };
