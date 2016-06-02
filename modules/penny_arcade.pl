3738e966027ad4f7b4015b421659396d97703358
Benjamin Kuperman <kuperman@acm.org>
2016-06-02 10:18:16 -0400

    Update Penny Arcade to SSL and new image URL

--- modules/penny_arcade.pl
+++ modules/penny_arcade.pl
@@ -1,8 +1,7 @@
 $COMIC{penny_arcade} = {
 			Title => 'Penny Arcade',
-			Base => 'http://www.penny-arcade.com',
+			Base => 'https://www.penny-arcade.com',
 			Page => '{Base}/comic',
-                        #Regex => qr@src="(http://art.penny-arcade.com/photos/[^\"]*?\.(gif|jpg))"@i,
-			Regex => qr@src="(http(s)?://[^/]*penny-arcade[^/]*/Comics/[^\"]*?\.(gif|png|jpeg|jpg))"@i,
+			Regex => qr@src="(http(s)?://[^/]*/Comics/[^/]*Pa-comics[^\"]*?\.(gif|png|jpeg|jpg))"@i,
                         TitleRegex => qr@<title>Penny Arcade - Comic -\s+(.+)</title>@i,
 		       };
