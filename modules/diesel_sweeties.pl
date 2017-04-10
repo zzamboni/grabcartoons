41aaff1ff8d61c00c0fdeb666b645fa31d35cab3
Benjamin Kuperman <kuperman@acm.org>
2017-04-10 12:38:32 -0700

    Minor update to diesel_sweeties

--- modules/diesel_sweeties.pl
+++ modules/diesel_sweeties.pl
@@ -1,7 +1,7 @@
 $COMIC{diesel_sweeties}={
 			 Title	=> 'Diesel Sweeties',
 			 Page	=> 'http://www.dieselsweeties.com/',
-			 Regex	=> qr(<img class="xomic"[^>]*src="((?:\/(h)?strips.*?)?[^>]*\.(?:png|gif|jpg|jpeg))")i,
+			 Regex	=> qr(<img class="xomic"[^>]*src="((?:\/(h)?strips.*?)[^>]*\.(?:png|gif|jpg|jpeg))")i,
 			 Prepend => '{Page}',
 			};
 
