f8726e84e2cbf53cc14ffa4325e721fbc0be31f3
Benjamin Kuperman <kuperman@acm.org>
2017-09-02 16:13:19 -0700

    Update Sluggy Freelance

--- modules/sluggy_freelance.pl
+++ modules/sluggy_freelance.pl
@@ -1,6 +1,6 @@
 $COMIC{sluggy_freelance} = {
 			    Title => 'Sluggy Freelance',
 			    Page => 'http://www.sluggy.com/',
-			    Regex => qr/SRC\s*=\s*"(.*\/comics\/.*?\.(gif|jpg))"/i,
-                            Prepend => '{Page}',
+			    Regex => qr/SRC\s*=\s*"(.*\/comics\/.*?\.(gif|jpg|jpeg|png))"/i,
+                            #Prepend => '{Page}',
 			   };
