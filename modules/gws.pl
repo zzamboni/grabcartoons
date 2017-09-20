df9f7e1be9ee735d049a4ec11d9ca21a9502bf4e
Benjamin Kuperman <kuperman@acm.org>
2017-09-19 21:37:18 -0700

    Update Girls With Slingshots

--- modules/gws.pl
+++ modules/gws.pl
@@ -1,6 +1,6 @@
 $COMIC{gws} = {
 		Title => 'Girls with Slingshots',
 		Page => 'http://www.girlswithslingshots.com/',
-		Regex => qr!src="(http://[^/]*?/comics/.*?)"!i,
-		ExtraImgAttrsRegex => qr!src="http://[^/]*?/comics/.*?".*?(title=".*?")!i,
+		Regex => qr!src="((http:)?//[^/]*?/comics/.*?)"!i,
+		ExtraImgAttrsRegex => qr!(title=".*?").*?src="(http:)?//[^/]*?/comics/.*?"!i,
 	       };
