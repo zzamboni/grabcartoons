9d5392118293312bfdaf6c4bbb92eddcdcd1013d
Benjamin Kuperman <kuperman@acm.org>
2020-12-05 15:57:08 -0800

    Fix for Robot Hugs

--- modules/robot_hugs.pl
+++ modules/robot_hugs.pl
@@ -1,8 +1,8 @@
 $COMIC{robot_hugs} = {
         Title => 'Robot Hugs',
         Page => 'http://www.robot-hugs.com/',
-        StartRegex => qr!<div id="comic">!,
-        Regex => qr!img src="([^"]*\/comics\/[^."]+\.(?:gif|png|jpg|jpeg))"!i,
-        TitleRegex => qr!img src.*comics.*alt="(.*?)"!,
+        StartRegex => qr!<div id="comic"!,
+        Regex => qr!img[^>]*src="([^"]*\/(?:comics|www.robot-hugs.com\/wp-content\/uploads)\/[^."]+\.(?:gif|png|jpg|jpeg))[^"]*"!i,
+        TitleRegex => qr!div.*comic.*img[^>]*src[^>]*alt="(.*?)"!,
         SkipLink => 1,
     };
