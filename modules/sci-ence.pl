b6d9d93d5bd578c19ab0fd80878cf34607135a4b
Diego Zamboni <diego@zzamboni.org>
2020-12-06 21:25:27 +0100

    sci-ence.org website is gone, module removed

--- modules/sci-ence.pl
+++
@@ -1,6 +0,0 @@
-$COMIC{'sci-ence'} = {
-		Title => 'sci-ence',
-		Page => 'http://sci-ence.org/',
-		Regex => qr!img[^<>]*src="(http://sci-ence\.org/comics/.*?)"!i,
-		TitleRegex => qr!comicpane.*title="(.*?)"!i,
-	       };
