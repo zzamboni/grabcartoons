4ebb85b02d24a94c8e140191a05cfd36f82ca54c
Diego Zamboni <diego@zzamboni.org>
2020-12-06 21:24:08 +0100

    partiallyclips website is gone, module removed.

--- modules/partiallyclips.pl
+++
@@ -1,9 +0,0 @@
-$COMIC{partiallyclips} = {
-			  Title => 'PartiallyClips',
-			  Base => 'http://www.partiallyclips.com',
-			  Page => '{Base}/',
-                          StartRegex => qr(\<div id="comic"\>),
-                          EndRegex => qr(\<div class="clear"\>),
-			  'NoShowTitle' => 1,
-                          #Regex => qr/IMG SRC="({Base}.*\/storage\/.*\.(gif|png|jpg))" border="0" Title="Click for larger version"/i,
-			 };
