a3f3363c27708e62c0cbf243678135c319336c93
Diego Zamboni <diego@zzamboni.org>
2020-12-07 13:17:59 +0100

    Hijinks Ensue has not been updated in 5 years, module removed.

--- modules/hijinksensue.pl
+++
@@ -1,7 +0,0 @@
-$COMIC{hijinksensue} = {
-		   Title => 'Hijinks Ensue',
-		   Page => 'http://hijinksensue.com/',
-                   StartRegex => '<div id="comic">',
-		   Regex => qr!img\s+src\s*=\s*\"(.*?(?:gif|png|jpg))"!i,
-                   ExtraImgAttrsRegex => qr!(alt=".*?")!i,
-		  };
