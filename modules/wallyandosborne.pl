8160425c8bf3ac8777e5097a498d05427aa4fda2
Diego Zamboni <diego@zzamboni.org>
2020-12-07 18:09:59 +0100

    wallyandosborne moved to Instagram, no easy way to parse

--- modules/wallyandosborne.pl
+++
@@ -1,6 +0,0 @@
-$COMIC{wallyandosborne} = {
-    Title => 'Wally and Osborne',
-    Page => 'http://www.wallyandosborne.com/',
-    Regex => qr!<img src=['"](http://(?:www\.)?wallyandosborne.com\/[^"']*/\d+-\d+-\d+-\d+\.(?:gif|png|jpg|jpeg))["']!i,
-    ExtraImgAttrsRegex => qr!(title=".*")!,
-};
