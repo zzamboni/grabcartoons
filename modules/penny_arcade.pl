d96586221da9ffba5377c703fb4208b527e04943
Benjamin Kuperman <kuperman@acm.org>
2022-11-05 15:40:31 -0700

    Update Penny Arcade for 3-panel layout

--- modules/penny_arcade.pl
+++ modules/penny_arcade.pl
@@ -2,6 +2,12 @@ $COMIC{penny_arcade} = {
 			Title => 'Penny Arcade',
 			Base => 'https://www.penny-arcade.com',
 			Page => '{Base}/comic',
-			Regex => qr@src="(http(s)?://[^/]*/comics/[^/]*[^\"]*?\.(gif|png|jpeg|jpg))"@i,
-                        TitleRegex => qr@<title>Penny Arcade - Comic -\s+(.+)</title>@i,
+			StartRegex => qr@<a id="comic-panels" class="three-panel" >@,
+			EndRegex => qr@</a>@,
+			SubstOnRegexResult => [
+			    [ qr@srcset=".*alt=""@, 'width="30%" alt=""', 1 ],
+			    [ qr@<div class="comic-panel">@, '', 1 ],
+			    [ qr@</div>@, '', 1 ]
+			],
+			TitleRegex => qr@<title>Penny Arcade - Comic -\s+(.+)</title>@i,
 		       };
